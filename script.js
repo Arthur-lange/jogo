afa09f40ef38fa928b2f9419c492dfa898e81c47
Arthur Lange <arthurlange0406@gmail.com>
2025-03-05 09:19:33 -0300

    adicionado html 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-a=1
+// pegar o elemento canvas pelo id
+const canvas = document.getElementById('jogo2D')
+
+//inicializar o canvas
+const ctx = canvas.getcontext('2d')
+
+//criar a função loop 
+function loop () {
+
+}
+
+loop ()
